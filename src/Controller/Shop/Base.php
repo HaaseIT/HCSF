c17449e9c0fd46e7576746934c699c7566f618af
Marcus Haase <mail@marcus.haase.name>
2016-01-15 15:18:36 +0100

    added shop Base controller

---
+++ src/Controller/Shop/Base.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+    HCSF - A multilingual CMS and Shopsystem
+    Copyright (C) 2014  Marcus Haase - mail@marcus.haase.name
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace HaaseIT\HCSF\Controller\Shop;
+
+
+class Base extends \HaaseIT\HCSF\Controller\Base
+{
+    public function __construct($C, $DB, $sLang)
+    {
+        parent::__construct($C, $DB, $sLang);
+        if (empty($C["enable_module_shop"]) || !$C["enable_module_shop"]) {
+            throw new \Exception(404);
+        }
+        $this->P = new \HaaseIT\HCSF\CorePage($C, $sLang);
+        $this->P->cb_pagetype = 'content';
+    }
+}
