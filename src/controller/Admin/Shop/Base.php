5419010c6ffb9b948151fa3cc268311c542982b5
Marcus Haase <mail@marcus.haase.name>
2016-01-15 12:26:36 +0100

    added admin/shop base controller

---
+++ src/controller/Admin/Shop/Base.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhaase
+ * Date: 14.01.16
+ * Time: 23:28
+ */
+
+namespace HaaseIT\HCSF\Controller\Admin\Shop;
+
+
+class Base extends \HaaseIT\HCSF\Controller\Admin\Base
+{
+    public function __construct($C, $DB, $sLang)
+    {
+        parent::__construct($C, $DB, $sLang);
+
+    }
+}
