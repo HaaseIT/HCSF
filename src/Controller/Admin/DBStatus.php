bcd72aac095a6f2c19512a8e3af3ad10d8717726
Marcus Haase <mail@marcus.haase.name>
2016-02-03 06:57:04 +0100

    added dbstatus functionality

--- src/Controller/Admin/DBStatus.php
+++ src/Controller/Admin/DBStatus.php
@@ -32,10 +32,10 @@ class DBStatus extends Base
         $this->P->cb_customdata['rows_textcat_lang'] = $hResult->rowCount();
         if ($this->P->cb_customdata['rows_textcat_lang'] > 0) {
             $aListSetting = [
-                ['title' => 'tcl_id', 'key' => 'tcl_id', 'width' => '7%', 'linked' => false,],
-                ['title' => 'tcl_tcid', 'key' => 'tcl_tcid', 'width' => '7%', 'linked' => false, 'escapehtmlspecialchars' => true,],
-                ['title' => 'tcl_lang', 'key' => 'tcl_lang', 'width' => '7%', 'linked' => false, 'escapehtmlspecialchars' => true,],
-                ['title' => 'tcl_text', 'key' => 'tcl_text', 'width' => '79%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'tcl_id', 'key' => 'tcl_id', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_tcid', 'key' => 'tcl_tcid', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_lang', 'key' => 'tcl_lang', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_text', 'key' => 'tcl_text', 'width' => '82%', 'linked' => false, 'escapehtmlspecialchars' => true,],
             ];
             $aData = $hResult->fetchAll();
             $this->P->cb_customdata['rows_textcat_lang_list'] = \HaaseIT\Tools::makeListtable(
@@ -50,45 +50,12 @@ class DBStatus extends Base
         $this->P->cb_customdata['rows_textcat_lang_archive'] = $hResult->rowCount();
         if ($this->P->cb_customdata['rows_textcat_lang_archive'] > 0) {
             $aListSetting = [
-                [
-                    'title' => 'tcla_timestamp',
-                    'key' => 'tcla_timestamp',
-                    'width' => '15%',
-                    'linked' => false,
-                ],
-                [
-                    'title' => 'tcla_id',
-                    'key' => 'tcla_id',
-                    'width' => '7%',
-                    'linked' => false,
-                ],
-                [
-                    'title' => 'tcl_id',
-                    'key' => 'tcl_id',
-                    'width' => '7%',
-                    'linked' => false,
-                ],
-                [
-                    'title' => 'tcl_tcid',
-                    'key' => 'tcl_tcid',
-                    'width' => '7%',
-                    'linked' => false,
-                    'escapehtmlspecialchars' => true,
-                ],
-                [
-                    'title' => 'tcl_lang',
-                    'key' => 'tcl_lang',
-                    'width' => '7%',
-                    'linked' => false,
-                    'escapehtmlspecialchars' => true,
-                ],
-                [
-                    'title' => 'tcl_text',
-                    'key' => 'tcl_text',
-                    'width' => '57%',
-                    'linked' => false,
-                    'escapehtmlspecialchars' => true,
-                ],
+                ['title' => 'tcla_timestamp', 'key' => 'tcla_timestamp', 'width' => '15%', 'linked' => false,],
+                ['title' => 'tcla_id', 'key' => 'tcla_id', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_id', 'key' => 'tcl_id', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_tcid', 'key' => 'tcl_tcid', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_lang', 'key' => 'tcl_lang', 'width' => '6%', 'linked' => false,],
+                ['title' => 'tcl_text', 'key' => 'tcl_text', 'width' => '61%', 'linked' => false, 'escapehtmlspecialchars' => true,],
             ];
             $aData = $hResult->fetchAll();
             $this->P->cb_customdata['rows_textcat_lang_archive_list'] = \HaaseIT\Tools::makeListtable($aListSetting,
@@ -98,21 +65,189 @@ class DBStatus extends Base
         if (isset($_GET['clearorphanedcontent'])) $DB->exec('DELETE FROM content_lang WHERE cl_cb NOT IN (SELECT cb_id FROM content_base)');
         $hResult = $DB->query('SELECT * FROM content_lang WHERE cl_cb NOT IN (SELECT cb_id FROM content_base)');
         $this->P->cb_customdata['rows_content_lang'] = $hResult->rowCount();
+        if ($this->P->cb_customdata['rows_content_lang'] > 0) {
+            $aListSetting = [
+                ['title' => 'cl_id', 'key' => 'cl_id', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_cb', 'key' => 'cl_cb', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_lang', 'key' => 'cl_lang', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_html', 'key' => 'cl_html', 'width' => '43%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'cl_keywords', 'key' => 'cl_keywords', 'width' => '13%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'cl_description', 'key' => 'cl_description', 'width' => '13%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'cl_title', 'key' => 'cl_title', 'width' => '13%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+            ];
+            $aData = $hResult->fetchAll();
+            $this->P->cb_customdata['rows_content_lang_list'] = \HaaseIT\Tools::makeListtable($aListSetting,
+                $aData, $twig);
+        }
 
         if (isset($_GET['clearorphanedcontentarchive'])) $DB->exec('DELETE FROM content_lang_archive WHERE cl_cb NOT IN (SELECT cb_id FROM content_base)');
         $hResult = $DB->query('SELECT * FROM content_lang_archive WHERE cl_cb NOT IN (SELECT cb_id FROM content_base)');
         $this->P->cb_customdata['rows_content_lang_archive'] = $hResult->rowCount();
+        if ($this->P->cb_customdata['rows_content_lang_archive'] > 0) {
+            $aListSetting = [
+                ['title' => 'cla_timestamp', 'key' => 'cla_timestamp', 'width' => '15%', 'linked' => false,],
+                ['title' => 'cla_id', 'key' => 'cla_id', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_id', 'key' => 'cl_id', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_cb', 'key' => 'cl_cb', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_lang', 'key' => 'cl_lang', 'width' => '6%', 'linked' => false,],
+                ['title' => 'cl_html', 'key' => 'cl_html', 'width' => '33%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'cl_keywords', 'key' => 'cl_keywords', 'width' => '10%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'cl_description', 'key' => 'cl_description', 'width' => '10%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                ['title' => 'cl_title', 'key' => 'cl_title', 'width' => '10%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+            ];
+            $aData = $hResult->fetchAll();
+            $this->P->cb_customdata['rows_content_lang_archive_list'] = \HaaseIT\Tools::makeListtable($aListSetting,
+                $aData, $twig);
+        }
+        if ($C['enable_module_shop']) {
+            if (isset($_GET['clearorphaneditems'])) {
+                $DB->exec('DELETE FROM item_lang WHERE itml_pid NOT IN (SELECT itm_id FROM item_base)');
+            }
+            $hResult = $DB->query('SELECT * FROM item_lang WHERE itml_pid NOT IN (SELECT itm_id FROM item_base)');
+            $this->P->cb_customdata['rows_item_lang'] = $hResult->rowCount();
+            if ($this->P->cb_customdata['rows_item_lang'] > 0) {
+                $aListSetting = [
+                    ['title' => 'itml_id', 'key' => 'itml_id', 'width' => '6%', 'linked' => false,],
+                    ['title' => 'itml_pid', 'key' => 'itml_pid', 'width' => '6%', 'linked' => false,],
+                    ['title' => 'itml_lang', 'key' => 'itml_lang', 'width' => '6%', 'linked' => false,],
+                    [
+                        'title' => 'itml_name_override',
+                        'key' => 'itml_name_override',
+                        'width' => '18%',
+                        'linked' => false,
+                        'escapehtmlspecialchars' => true,
+                    ],
+                    [
+                        'title' => 'itml_text1',
+                        'key' => 'itml_text1',
+                        'width' => '32%',
+                        'linked' => false,
+                        'escapehtmlspecialchars' => true,
+                    ],
+                    [
+                        'title' => 'itml_text2',
+                        'key' => 'itml_text2',
+                        'width' => '32%',
+                        'linked' => false,
+                        'escapehtmlspecialchars' => true,
+                    ],
+                ];
+                $aData = $hResult->fetchAll();
+                $this->P->cb_customdata['rows_item_lang_list'] = \HaaseIT\Tools::makeListtable($aListSetting,
+                    $aData, $twig);
+            }
 
-        if (isset($_GET['clearorphaneditems'])) $DB->exec('DELETE FROM item_lang WHERE itml_pid NOT IN (SELECT itm_id FROM item_base)');
-        $hResult = $DB->query('SELECT * FROM item_lang WHERE itml_pid NOT IN (SELECT itm_id FROM item_base)');
-        $this->P->cb_customdata['rows_item_lang'] = $hResult->rowCount();
+            if (isset($_GET['clearorphaneditemgroups'])) {
+                $DB->exec('DELETE FROM itemgroups_text WHERE itmgt_pid NOT IN (SELECT itmg_id FROM itemgroups_base)');
+            }
+            $hResult = $DB->query('SELECT * FROM itemgroups_text WHERE itmgt_pid NOT IN (SELECT itmg_id FROM itemgroups_base)');
+            $this->P->cb_customdata['rows_itemgroups_text'] = $hResult->rowCount();
+            if ($this->P->cb_customdata['rows_itemgroups_text'] > 0) {
+                $aListSetting = [
+                    ['title' => 'itmgt_id', 'key' => 'itmgt_id', 'width' => '6%', 'linked' => false,],
+                    ['title' => 'itmgt_pid', 'key' => 'itmgt_pid', 'width' => '6%', 'linked' => false,],
+                    ['title' => 'itmgt_lang', 'key' => 'itmgt_lang', 'width' => '6%', 'linked' => false,],
+                    [
+                        'title' => 'itmgt_shorttext',
+                        'key' => 'itmgt_shorttext',
+                        'width' => '41%',
+                        'linked' => false,
+                        'escapehtmlspecialchars' => true,
+                    ],
+                    [
+                        'title' => 'itmgt_details',
+                        'key' => 'itmgt_details',
+                        'width' => '41%',
+                        'linked' => false,
+                        'escapehtmlspecialchars' => true,
+                    ],
+                ];
+                $aData = $hResult->fetchAll();
+                $this->P->cb_customdata['rows_itemgroups_text_list'] = \HaaseIT\Tools::makeListtable($aListSetting,
+                    $aData, $twig);
+            }
 
-        if (isset($_GET['clearorphaneditemgroups'])) $DB->exec('DELETE FROM itemgroups_text WHERE itmgt_pid NOT IN (SELECT itmg_id FROM itemgroups_base)');
-        $hResult = $DB->query('SELECT * FROM itemgroups_text WHERE itmgt_pid NOT IN (SELECT itmg_id FROM itemgroups_base)');
-        $this->P->cb_customdata['rows_itemgroups_text'] = $hResult->rowCount();
-
-        if (isset($_GET['clearorphanedorderitems'])) $DB->exec('DELETE FROM orders_items WHERE oi_o_id  NOT IN (SELECT o_id FROM orders)');
-        $hResult = $DB->query('SELECT * FROM orders_items WHERE oi_o_id  NOT IN (SELECT o_id FROM orders)');
-        $this->P->cb_customdata['rows_orders_items'] = $hResult->rowCount();
+            if (isset($_GET['clearorphanedorderitems'])) {
+                $DB->exec('DELETE FROM orders_items WHERE oi_o_id  NOT IN (SELECT o_id FROM orders)');
+            }
+            $hResult = $DB->query('SELECT * FROM orders_items WHERE oi_o_id  NOT IN (SELECT o_id FROM orders)');
+            $this->P->cb_customdata['rows_orders_items'] = $hResult->rowCount();
+            if ($this->P->cb_customdata['rows_orders_items'] > 0) {
+                if ($C['vat_disable']) {
+                    $aListSetting = [
+                        ['title' => 'oi_id', 'key' => 'oi_id', 'width' => '8%', 'linked' => false,],
+                        ['title' => 'oi_o_id', 'key' => 'oi_o_id', 'width' => '9%', 'linked' => false,],
+                        ['title' => 'oi_cartkey', 'key' => 'oi_cartkey', 'width' => '10%', 'linked' => false,],
+                        ['title' => 'oi_amount', 'key' => 'oi_amount', 'width' => '9%', 'linked' => false,],
+                        ['title' => 'oi_vat', 'key' => 'oi_vat', 'width' => '8%', 'linked' => false,],
+                        ['title' => 'oi_rg', 'key' => 'oi_rg', 'width' => '8%', 'linked' => false,],
+                        ['title' => 'oi_rg_rebate', 'key' => 'oi_rg_rebate', 'width' => '9%', 'linked' => false,],
+                        [
+                            'title' => 'oi_price_brutto_list',
+                            'key' => 'oi_price_brutto_list',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        [
+                            'title' => 'oi_price_brutto_sale',
+                            'key' => 'oi_price_brutto_sale',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        [
+                            'title' => 'oi_price_brutto_rebated',
+                            'key' => 'oi_price_brutto_rebated',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        [
+                            'title' => 'oi_price_brutto_use',
+                            'key' => 'oi_price_brutto_use',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        //['title' => 'oi_img', 'key' => 'oi_img', 'width' => '41%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                    ];
+                } else {
+                    $aListSetting = [
+                        ['title' => 'oi_id', 'key' => 'oi_id', 'width' => '8%', 'linked' => false,],
+                        ['title' => 'oi_o_id', 'key' => 'oi_o_id', 'width' => '9%', 'linked' => false,],
+                        ['title' => 'oi_cartkey', 'key' => 'oi_cartkey', 'width' => '13%', 'linked' => false,],
+                        ['title' => 'oi_amount', 'key' => 'oi_amount', 'width' => '9%', 'linked' => false,],
+                        ['title' => 'oi_vat', 'key' => 'oi_vat', 'width' => '8%', 'linked' => false,],
+                        ['title' => 'oi_rg', 'key' => 'oi_rg', 'width' => '8%', 'linked' => false,],
+                        ['title' => 'oi_rg_rebate', 'key' => 'oi_rg_rebate', 'width' => '9%', 'linked' => false,],
+                        [
+                            'title' => 'oi_price_netto_list',
+                            'key' => 'oi_price_netto_list',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        [
+                            'title' => 'oi_price_netto_sale',
+                            'key' => 'oi_price_netto_sale',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        [
+                            'title' => 'oi_price_netto_rebated',
+                            'key' => 'oi_price_netto_rebated',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        [
+                            'title' => 'oi_price_netto_use',
+                            'key' => 'oi_price_netto_use',
+                            'width' => '9%',
+                            'linked' => false,
+                        ],
+                        //['title' => 'oi_img', 'key' => 'oi_img', 'width' => '41%', 'linked' => false, 'escapehtmlspecialchars' => true,],
+                    ];
+                }
+                $aData = $hResult->fetchAll();
+                $this->P->cb_customdata['rows_orders_items_list'] = \HaaseIT\Tools::makeListtable($aListSetting,
+                    $aData, $twig);
+            }
+        }
     }
 }
