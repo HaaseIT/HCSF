5fb009daae0fbdb8cdf4f151e42a0249624caff5
Marcus Haase <mail@marcus.haase.name>
2016-06-28 21:18:36 +0200

    completed tests for items->calcPrice()

--- tests/Shop/ItemsTest.php
+++ tests/Shop/ItemsTest.php
@@ -5,6 +5,8 @@ class ItemsTest extends TestCase
 {
     public function testCalcPrice()
     {
+        bcscale(6);
+
         $C = [
             'vat' => [
                 'full' => 0,
@@ -18,16 +20,18 @@ class ItemsTest extends TestCase
 
         // regular price, no rebate, vat disabled
         $aData = [
-            'itm_price' => 11.11,
+            'itm_price' => '11.11',
             'itm_rg' => '01',
             'itm_vatid' => 'full',
         ];
 
         $aPrice = $items->calcPrice($aData);
 
-        $this->assertEquals(11.11, $aPrice['netto_use']);
-        $this->assertEquals(11.11, $aPrice['brutto_use']);
-        $this->assertEquals(11.11, $aPrice['netto_list']);
+        $this->assertEquals('11.11', $aPrice['netto_use']);
+        $this->assertEquals('11.11', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertArrayNotHasKey('netto_sale', $aPrice);
+        $this->assertArrayNotHasKey('netto_rebated', $aPrice);
 
         // set vat to normal values
         $C = [
@@ -45,46 +49,172 @@ class ItemsTest extends TestCase
 
         // regular price, no rebate, reduced vat
         $aData = [
-            'itm_price' => 11.11,
+            'itm_price' => '11.11',
             'itm_rg' => '01',
             'itm_vatid' => 'reduced',
         ];
 
         $aPrice = $items->calcPrice($aData);
 
-        $this->assertEquals(11.11, $aPrice['netto_use']);
-        $this->assertEquals(11.8877, $aPrice['brutto_use']);
-        $this->assertEquals(11.11, $aPrice['netto_list']);
+        $this->assertEquals('11.11', $aPrice['netto_use']);
+        $this->assertEquals('11.8877', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertArrayNotHasKey('netto_sale', $aPrice);
+        $this->assertArrayNotHasKey('netto_rebated', $aPrice);
 
         // regular price, no rebate, full vat
         $aData = [
-            'itm_price' => 11.11,
+            'itm_price' => '11.11',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+        ];
+
+        $aPrice = $items->calcPrice($aData);
+
+        $this->assertEquals('11.11', $aPrice['netto_use']);
+        $this->assertEquals('13.2209', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertArrayNotHasKey('netto_sale', $aPrice);
+        $this->assertArrayNotHasKey('netto_rebated', $aPrice);
+
+        // item sale
+        $aData = [
+            'itm_price' => '11.11',
             'itm_rg' => '01',
             'itm_vatid' => 'full',
             'itm_data' => [
                 'sale' => [
-//                    'start' => date("Ymd") - 1,
-//                    'end' => date("Ymd") + 1,
-//                    'price' => 90.10,
+                    'start' => date("Ymd") - 1,
+                    'end' => date("Ymd") + 1,
+                    'price' => '9.10',
                 ],
             ],
         ];
 
         $aPrice = $items->calcPrice($aData);
 
-        $this->assertEquals(11.11, $aPrice['netto_use']);
-        $this->assertEquals(13.2209, $aPrice['brutto_use']);
-        $this->assertEquals(11.11, $aPrice['netto_list']);
+        $this->assertEquals('9.1', $aPrice['netto_use']);
+        $this->assertEquals('9.1', $aPrice['netto_sale']);
+        $this->assertEquals('10.829', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertArrayNotHasKey('netto_rebated', $aPrice);
 
-        // test item sale
+        // item sale too  late
+        $aData = [
+            'itm_price' => '11.11',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+            'itm_data' => [
+                'sale' => [
+                    'start' => date("Ymd") - 5,
+                    'end' => date("Ymd") - 1,
+                    'price' => '9.10',
+                ],
+            ],
+        ];
 
-        // test item sale too early / too late
+        $aPrice = $items->calcPrice($aData);
+
+        $this->assertEquals('11.11', $aPrice['netto_use']);
+        $this->assertEquals('13.2209', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertArrayNotHasKey('netto_sale', $aPrice);
+        $this->assertArrayNotHasKey('netto_rebated', $aPrice);
+
+        // test item sale too early
+        $aData = [
+            'itm_price' => '11.11',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+            'itm_data' => [
+                'sale' => [
+                    'start' => date("Ymd") + 1,
+                    'end' => date("Ymd") + 5,
+                    'price' => '9.10',
+                ],
+            ],
+        ];
+
+        $aPrice = $items->calcPrice($aData);
+
+        $this->assertEquals('11.11', $aPrice['netto_use']);
+        $this->assertEquals('13.2209', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertArrayNotHasKey('netto_sale', $aPrice);
+        $this->assertArrayNotHasKey('netto_rebated', $aPrice);
+
+        // init session for rebate testing
+        $_SESSION["user"] = [
+            'cust_group' => 'grosskunde',
+        ];
 
         // test rebate group
+        $aData = [
+            'itm_price' => '11.11',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+        ];
+
+        $aPrice = $items->calcPrice($aData);
+
+        $this->assertEquals('10.3323', $aPrice['netto_use']);
+        $this->assertEquals('12.295437', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertEquals('10.3323', $aPrice['netto_rebated']);
+        $this->assertArrayNotHasKey('netto_sale', $aPrice);
 
         // test sale/rebate group best price finding
+        // rebate best price
+        $aData = [
+            'itm_price' => '11.11',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+            'itm_data' => [
+                'sale' => [
+                    'start' => date("Ymd") - 1,
+                    'end' => date("Ymd") + 1,
+                    'price' => '10.99',
+                ],
+            ],
+        ];
 
-        // test non valid price
-        
+        $aPrice = $items->calcPrice($aData);
+        $this->assertEquals('10.3323', $aPrice['netto_use']);
+        $this->assertEquals('12.295437', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertEquals('10.3323', $aPrice['netto_rebated']);
+        $this->assertEquals('10.99', $aPrice['netto_sale']);
+
+        // sale best price
+        $aData = [
+            'itm_price' => '11.11',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+            'itm_data' => [
+                'sale' => [
+                    'start' => date("Ymd") - 1,
+                    'end' => date("Ymd") + 1,
+                    'price' => '9.10',
+                ],
+            ],
+        ];
+
+        $aPrice = $items->calcPrice($aData);
+        $this->assertEquals('9.1', $aPrice['netto_use']);
+        $this->assertEquals('10.829', $aPrice['brutto_use']);
+        $this->assertEquals('11.11', $aPrice['netto_list']);
+        $this->assertEquals('10.3323', $aPrice['netto_rebated']);
+        $this->assertEquals('9.1', $aPrice['netto_sale']);
+
+        // non valid price
+        $aData = [
+            'itm_price' => 'asdf',
+            'itm_rg' => '01',
+            'itm_vatid' => 'full',
+        ];
+
+        $aPrice = $items->calcPrice($aData);
+        $this->assertFalse($aPrice);
+
     }
 }
