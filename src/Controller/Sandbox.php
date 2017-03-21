60597aa9483c1e1b6dbcc31e4f9c94e37939e345
Marcus Haase <mail@marcus.haase.name>
2017-03-21 17:40:08 +0100

    emptied

--- src/Controller/Sandbox.php
+++ src/Controller/Sandbox.php
@@ -25,76 +25,10 @@ use Zend\ServiceManager\ServiceManager;
 
 class Sandbox extends Base
 {
-    /**
-     * @var \Doctrine\ORM\EntityManager
-     */
-    private $entityManager;
 
     public function __construct(ServiceManager $serviceManager)
     {
         parent::__construct($serviceManager);
-        $this->entityManager = $serviceManager->get('entitymanager');
     }
 
-    public function preparePage()
-    {
-        $this->P = new \HaaseIT\HCSF\CorePage($this->serviceManager);
-        $this->P->cb_pagetype = 'content';
-
-        $html = '<pre>';
-
-        /*
-        $this->entityManager->getConnection()
-            ->getConfiguration()
-            ->setSQLLogger(new \Doctrine\DBAL\Logging\EchoSQLLogger())
-        ;
-        */
-
-        //$customer = $this->entityManager->find(ENTITY_CUSTOMER, 1);
-
-        //$this->P->oPayload->cl_html = \HaaseIT\Toolbox\Tools::debug($customer->getName(), 'customername', true);
-
-
-        $dql = "SELECT l, b FROM ".ENTITY_USERPAGE_LANG." l JOIN l.basepage b WHERE l.language = ?1 AND b.key = ?2";
-        //$dql = "SELECT l FROM ".ENTITY_USERPAGE_LANG." l";
-        //die($dql);
-        try
-        {
-            $pages = $this->entityManager->createQuery($dql)
-                ->setParameter(1, 'de')
-                ->setParameter(2, '/index.html')
-                ->setMaxResults(10)
-                ->getResult();
-            foreach ($pages as $page) {
-                $html .= 'base id:'.$page->getBasepage()->getId().PHP_EOL;
-                $html .= 'base key:'.$page->getBasepage()->getKey().PHP_EOL;
-                $html .= 'base group:'.$page->getBasepage()->getGroup().PHP_EOL;
-                $html .= 'base pagetype:'.$page->getBasepage()->getPagetype().PHP_EOL;
-                $html .= 'base pageconfig:'.$page->getBasepage()->getPageconfig().PHP_EOL;
-                $html .= 'base subnav:'.$page->getBasepage()->getSubnav().PHP_EOL;
-
-                $html .= 'lang id:'.$page->getId().PHP_EOL;
-                $html .= 'lang language:'.$page->getLanguage().PHP_EOL;
-                $html .= 'lang html:'.$page->getHtml().PHP_EOL;
-                $html .= 'lang keywords:'.$page->getKeywords().PHP_EOL;
-                $html .= 'lang description:'.$page->getDescription().PHP_EOL;
-                $html .= 'lang title:'.$page->getTitle().PHP_EOL;
-
-                $page->getBasepage()->setGroup('testi');
-                $page->setKeywords('testi kel');
-
-                $this->serviceManager->get('entitymanager')->persist($page);
-                $this->serviceManager->get('entitymanager')->flush();
-
-
-            }
-        }
-        catch (\Exception $e)
-        {
-            $html .= \HaaseIT\Toolbox\Tools::debug($e, 'exception', true);
-        }
-
-        $this->P->oPayload->cl_html = $html.'</pre>';
-
-    }
 }
